a3320ce0f24f4f17ededdb4eef1bde22c1a41266
Luks-code <lucas123gamerpro@gmail.com>
2023-04-26 19:42:27 -0300

    Text to Speech & Loading Feature

--- src/components/Wali.jsx
+++ src/components/Wali.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { OpenAIApi, Configuration } from "openai";
 import { Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
+import Spinner from "react-bootstrap/Spinner";
 
 export const Wali = () => {
+  const element = <FontAwesomeIcon icon={faVolumeHigh} />;
+
   const [message, setMessage] = useState("");
 
   const [question, setQuestion] = useState("");
+
+  const [loading, setLoading] = useState(false);
 
   const openai = new OpenAIApi(
     new Configuration({
@@ -13,32 +20,48 @@ export const Wali = () => {
     })
   );
 
+  const textToSpeech = (e) => {
+    speechSynthesis.speak(new SpeechSynthesisUtterance(message));
+  };
+
   const handleChange = (e) => {
     setQuestion(e.target.value);
   };
 
   const respuesta = async (e) => {
+    setLoading(true);
     const res = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: question }],
     });
     setMessage(res.data.choices[0].message.content);
+    setLoading(false);
   };
 
   return (
     <div>
-      <Form.Group className="mb-3 mt-3">
-        <Form.Control
-          type="text"
-          name="name"
-          placeholder="Escribir pregunta"
-          onChange={handleChange}
-          required
-        />
-      </Form.Group>
-      <button onClick={respuesta} className="btn btn-success">
-        ENTER
-      </button>
+      <div className="group">
+        <Form.Group className="mb-3 mt-3">
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Escribir pregunta"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <button onClick={respuesta} className="btn btn-success">
+          ENTER
+        </button>
+        {message && (
+          <button onClick={textToSpeech} className="btn btn-success">
+            {element}
+          </button>
+        )}
+      </div>
+      {loading && (
+        <Spinner animation="border" role="status" variant="light"></Spinner>
+      )}
       <p className="respuesta">{message}</p>
     </div>
   );
